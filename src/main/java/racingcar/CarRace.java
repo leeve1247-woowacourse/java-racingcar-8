9f94649e50dd260f186b3946463eb9c63425f71c
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-21 23:13:29 +0900

    feat(core): 경주 기능 구현

--- src/main/java/racingcar/CarRace.java
+++ src/main/java/racingcar/CarRace.java
@@ -1,15 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.config.RacingGameConfig;
 import racingcar.view.ConsoleView;
 
 public class CarRace {
+    private final RacingGameConfig racingGameConfig;
+    private final List<Round> rounds = new ArrayList<>();
+
     public CarRace(RacingGameConfig racingGameConfig) {
+        this.racingGameConfig = racingGameConfig;
     }
 
-    public List<String> play(ConsoleView consoleView) {
-        return new ArrayList<>();
+    public RaceRecord play(ConsoleView consoleView) {
+        List<Car> cars = racingGameConfig.getCars();
+        int round = 1;
+
+        while (round <= racingGameConfig.getTryCount()) {
+            cars.forEach(car -> {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                if (randomNumber >= 4) {
+                    car.moveForward();
+                }
+            });
+            rounds.add(new Round(cars));
+            round++;
+        }
+
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        List<String> winners = cars.stream().filter(car -> car.getPosition() == maxPosition).map(Car::getName).toList();
+
+        return new RaceRecord(winners, rounds);
     }
 }
