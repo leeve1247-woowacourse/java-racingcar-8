8825531703ea27bdedcc324e92f963adae7f3edb
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-21 21:33:41 +0900

    refactor(input): 미사용 라이브러리 삭제

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -4,9 +4,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
 import racingcar.UserInput;
 
 @SuppressWarnings("NonAsciiCharacters")
