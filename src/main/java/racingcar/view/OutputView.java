9d336bc54075a495fa362ab1da62c8bf58f51b3b
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-21 20:40:37 +0900

    feat(output): 최종 결과 메시지 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,10 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
+
+    public void printResult(List<String> winners) {
+        System.out.println("최종 우승자: " + String.join(", ", winners));
+    }
 }
