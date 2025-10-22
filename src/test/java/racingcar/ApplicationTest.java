84a3dc71b1d98fe7db500f2087d04e099326e055
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-22 20:22:13 +0900

    test(app): 기능 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,9 +1,7 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
@@ -39,10 +37,22 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    void 기능_테스트_3(){
+        int valueOrder = 2418273;
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("aaaa,bbb,ccc,ddd,eee,ff,gg,h,aa", "22");
+                    assertThat(output())
+                            .contains("최종 우승자 : eee, h, aa")
+                            .contains("eee : "+"-".repeat(22))
+                            .contains("h : "+"-".repeat(22))
+                            .contains("aa : "+"-".repeat(22))
+                            .doesNotContain("eee : "+"-".repeat(23))
+                            .doesNotContain("h : "+"-".repeat(23))
+                            .doesNotContain("aa : "+"-".repeat(23))
+                    ;
+                },
+                MOVING_FORWARD, testUtil.movementPattern(valueOrder)
         );
     }
 
